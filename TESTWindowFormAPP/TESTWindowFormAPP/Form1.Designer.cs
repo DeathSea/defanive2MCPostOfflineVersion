36a8cb620567ee289fa32dc340bb6e3c0c2129fb
DeathSea <coder.deathsea@gmail.com>
2016-07-31 15:04:34 +0800

    search program vs2015 project

---
+++ TESTWindowFormAPP/TESTWindowFormAPP/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace TESTWindowFormAPP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SearchStr = new System.Windows.Forms.TextBox();
+            this.Start = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.文件名 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.上下文 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // SearchStr
+            // 
+            this.SearchStr.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.SearchStr.Location = new System.Drawing.Point(13, 13);
+            this.SearchStr.Name = "SearchStr";
+            this.SearchStr.Size = new System.Drawing.Size(545, 21);
+            this.SearchStr.TabIndex = 0;
+            // 
+            // Start
+            // 
+            this.Start.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Start.Location = new System.Drawing.Point(564, 13);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 23);
+            this.Start.TabIndex = 1;
+            this.Start.Text = "button1";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(12, 40);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(627, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.文件名,
+            this.上下文});
+            this.listView1.Location = new System.Drawing.Point(13, 69);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(627, 293);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.resultListBox_MouseDoubleClick);
+            // 
+            // 文件名
+            // 
+            this.文件名.Text = "文件名";
+            // 
+            // 上下文
+            // 
+            this.上下文.Text = "上下文";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 374);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.SearchStr);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox SearchStr;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader 文件名;
+        private System.Windows.Forms.ColumnHeader 上下文;
+    }
+}
+
---
+++ TESTWindowFormAPP/TESTWindowFormAPP/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TESTWindowFormAPP
+{
+    public partial class Form1 : Form
+    {
+        List<String> Result;
+        List<String> ContextS;
+        public Form1()
+        {
+            InitializeComponent();
+            Result = new List<String>();
+            ContextS = new List<String>();
+            progressBar1.Maximum = 100;
+            progressBar1.Minimum = 0;
+            progressBar1.Value = 0;
+            progressBar1.Step = 1;
+            backgroundWorker1.WorkerReportsProgress = true;
+            listView1.View = View.Details;
+            listView1.GridLines = true;
+
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+
+            String SearchString = SearchStr.Text;
+
+            if (SearchString.Length != 0) {
+                Result.Clear();
+                ContextS.Clear();
+                listView1.Items.Clear();
+                backgroundWorker1.RunWorkerAsync(SearchString);
+
+            }
+            //ShowDialog();
+        }
+
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            String S = (String)e.Argument;
+            string currentPath = System.Environment.CurrentDirectory;
+            var files = Directory.GetFiles(currentPath, "*.html");
+            int all = files.Length;
+            float ii = 1;
+
+            foreach (var file in files)
+            {
+                try
+                {   // Open the text file using a stream reader.
+                    using (StreamReader sr = new StreamReader(file))
+                    {
+                        String line = sr.ReadToEnd();
+                        int process = (int)((ii / all) * 100);
+                        //System.Diagnostics.Debug.WriteLine(String.Format("{0}", process));
+                        //MessageBox.Show(String.Format("{0}",process));
+                        backgroundWorker1.ReportProgress(process);
+                        int pos = line.IndexOf(S);
+                        if (pos != -1)
+                        {
+                            Result.Add(file);
+                            ContextS.Add(line.Substring(pos - 25 <0?0:pos - 25, 50));
+                        }
+                        ii += 1;
+                    }
+                }
+                catch (Exception ep)
+                {
+                    Console.WriteLine("The file could not be read:");
+                    Console.WriteLine(ep.Message);
+                }
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            for (int index =0;index<Result.Count;index++)
+            {
+                String R = Result[index];
+                ListViewItem Item = new ListViewItem();
+                ListViewItem.ListViewSubItem SubItem = new ListViewItem.ListViewSubItem();
+                SubItem.Text = ContextS[index];
+                Item.SubItems.Add(SubItem);
+                listView1.Items.Add(Item);
+                Item.Text = R;
+            }
+            listView1.EndUpdate();
+        }
+
+
+
+        private void resultListBox_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (listView1.SelectedItems.Count != 0)
+            {
+                String Value = listView1.SelectedItems[0].Text;
+                //MessageBox.Show(Value);
+                System.Diagnostics.Process.Start(Value);
+            }
+
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
